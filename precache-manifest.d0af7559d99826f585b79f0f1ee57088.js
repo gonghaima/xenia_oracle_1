ad95416e48f647969a9b93999a5e4b23da4f543b
Steven <gonghaima@hotmail.com>
2020-06-06 18:57:57 +1000

    Updates

---
+++ precache-manifest.d0af7559d99826f585b79f0f1ee57088.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8fa4b9af436c99243fadfd9c8836e7",
+    "url": "/xenia_oracle_1/index.html"
+  },
+  {
+    "revision": "15ada11342039e93ee42",
+    "url": "/xenia_oracle_1/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "61010eedb5b5b4980f9c",
+    "url": "/xenia_oracle_1/static/css/main.39a51d9d.chunk.css"
+  },
+  {
+    "revision": "15ada11342039e93ee42",
+    "url": "/xenia_oracle_1/static/js/2.0b3c9cdc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/xenia_oracle_1/static/js/2.0b3c9cdc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61010eedb5b5b4980f9c",
+    "url": "/xenia_oracle_1/static/js/main.43bb4dca.chunk.js"
+  },
+  {
+    "revision": "b374916e8c2da8203964",
+    "url": "/xenia_oracle_1/static/js/runtime-main.842d8e57.js"
+  },
+  {
+    "revision": "aecbcb6b7dbb99b45fa861f50d7dbb85",
+    "url": "/xenia_oracle_1/static/media/hotel-what-is-hotel-pms.aecbcb6b.jpg"
+  },
+  {
+    "revision": "9676248c814284a542916c203b134158",
+    "url": "/xenia_oracle_1/static/media/oracle-cloud.9676248c.jpg"
+  },
+  {
+    "revision": "a7c7c89ff1de8394646111a3304e31a6",
+    "url": "/xenia_oracle_1/static/media/xenia-head-img.a7c7c89f.jpg"
+  }
+]);
