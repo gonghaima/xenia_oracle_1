fc23fd657eb7cf41182edab6a98b6fab24944f75
Steven <gonghaima@hotmail.com>
2020-08-03 20:25:39 +1000

    revert

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -30,7 +30,7 @@ export default () => {
         <div className="full_width flex-center flex-end">
 
           <Link to="/">Home</Link>
-          <Link to="features">Features & Benefits</Link>
+          <Link to="/features">Features & Benefits</Link>
           <Link to="/">About us</Link>
           <Link to="/">Services</Link>
           <Link to="/">FAQ'S</Link>
@@ -44,8 +44,8 @@ export default () => {
       </div>
 
       <Router>
-        <Home path={process.env.PUBLIC_URL + '/'} header={header} />
-        <Features path={process.env.PUBLIC_URL + '/features'} header={header} />
+        <Home path="/" header={header} />
+        <Features path="/features" header={header} />
       </Router>
 
 
