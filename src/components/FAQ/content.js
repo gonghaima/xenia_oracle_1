7e5bf35194d81da74692b9672eb8f5c4eaa2332c
Steven <gonghaima@hotmail.com>
2020-09-05 08:11:17 +1000

    FAQ - heading and top QA

---
+++ src/components/FAQ/content.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        title:'How easy is Xenia Suite to use?',
+        content:'Xenia Suite is designed to make your life easier. We can implement and train your team on Xenia Suite in a matter of days, taking care of the day to day so you can concentrate on what matters most.'
+    },
+    {
+        title: 'Is local support available?',
+        content: 'We have two New Zealand support centres located in Auckland and Queenstown. Once you’re part of the Xenia Suite family we offer 24/7 phone and online support, as well as on-site training as required.'
+    },
+    {
+        title: 'Can Xenia Suite be used on a tablet?',
+        content: 'Xenia Suite has a full range of mobile applications including Reception, Housekeeping and Laundry, Maintenance, Minibar and a Guest Application that can run on tablets or smartphones.'
+    },
+    {
+        title: 'Does Xenia Suite integrate with a fully featured accounting system?',
+        content: 'Xenia Suite comes with fully integrated MYOB Exo Accounting and CRM. It is designed as an end-to-end system - from Bookings, Front-Desk and Point of Sale through to Operations and Function Management, Financial Management and Reporting.'
+    },
+    {
+        title: 'Does Xenia Suite have to be in the cloud?',
+        content: 'We will configure Xenia Suite to run on the platform that suits you. Be that in-house or in the cloud.'
+    },
+    {
+        title: 'Can Xenia Suite grow as my business grows?',
+        content: 'Yes, absolutely! Xenia Suite is configured for mid-range properties (50 rooms plus) and is scalable and suitable for larger sites up to 130 rooms.Xenia Suite also offers multiple property functionality that can enable individual or centralised back- office accounting.'
+    },
+    {
+        title: 'How much does Xenia Suite cost?',
+        content: 'Your hotel management system is an integral part of your business, and we understand you want to invest in the right system for you. At Xenia Suite we offer flexible pricing options based on the size of your property and your occupancy rates.Contact us today for a no- obligation quote.'
+    }
+];
--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -1,14 +1,11 @@
 
 import React, { useEffect, useState } from 'react';
-import f1Img from '../../images/features/feature-1.jpg';
-import f2Img from '../../images/features/feature-2.jpg';
-import f3Img from '../../images/features/feature-3.jpg';
-import f4Img from '../../images/features/feature-4.jpg';
-import f5Img from '../../images/features/feature-5.jpg';
-import f6Img from '../../images/features/feature-6.jpg';
+import { data } from './content';
 
 export default ({ header }) => {
     const [main, setMain] = useState();
+
+
 
     useEffect(() => {
 
@@ -22,32 +19,29 @@ export default ({ header }) => {
     }, [main]);
     return (
         <>
-            <section className="head-section-features">
+            <section className="head-section-faq">
                 <div className="head-section-content">
-                    <h2>Xenia Suite Features</h2>
+                    <h2>Xenia Suite</h2>
                 </div>
             </section>
-            <section className="features-content-section">
-                <h2>Technology you can trust</h2>
+            <section className="faq-content-section">
+                <h2>Frequently Asked Questions</h2>
                 <p>
-                    <i>
-                        “We’ve been using Xenia Suite to run Pavilion front and back office for 16 years now, and whilst Xenia stands out for its flexibility and functionality, it’s the
-                        user-friendliness and ease of training for staff that is really important to running smoothly. The outstanding support and responsiveness from the Xenia
-
-                        Suite team all these years also makes a big difference.”
-                    </i>
+                    Here you’ll find answers to our most common questions. Still want to know more? Get in touch to chat with our local team.
                 </p>
-                <h4>Debbie Horncastle, General Manager Pavilions Hotel Christchurch, New Zealand</h4>
-            </section>
-            <section className="main-section-features">
-                <div className="side">
-                    <img alt="f1-img" src={f1Img} />
+                <div className="button-container">
+                    <button>Contact Us</button>
                 </div>
-                <main>
-                    <h4>User-friendly systems</h4>
-                    <p>Xenia Suite is designed to make your life easier. We have made our system easy to
-                    implement and use, so you and your team can focus on keeping your guests happy.
-                    You and your team will benefit from:
+            </section>
+            <section className="main-section-faq">
+                {
+                    data.map(d => <main><h5>{d.title}</h5><p>{d.content}</p></main>)
+                }
+                {/* <main>
+                    <h5>How easy is Xenia Suite to use?</h5>
+                    <p>
+                        Xenia Suite is designed to make your life easier. We can implement and train your team on Xenia Suite in a matter of days, taking care of the
+                        day to day so you can concentrate on what matters most.
                     </p>
                     <ul>
                         <li>
@@ -66,156 +60,8 @@ export default ({ header }) => {
                             Training from the Xenia Suite team, and 24/7 access to our online knowledge database and instructional videos.
                         </li>
                     </ul>
-                </main>
+                </main> */}
             </section>
-            <section className="main-section-features">
-                <div className="side">
-                    <img alt="f2-img" src={f2Img} />
-                </div>
-                <main>
-                    <h4>Enhanced check-in/check-out capabilities</h4>
-                    <p>
-                        Empower your guests through the Xenia Suite mobile app. Guests can manage self-check-
-                        in / check-out, receive and send alerts for delays and emergencies; and manage their
-                        booking preferences through a touch of a button.
-                    </p>
-                    <p>
-                        The Xenia Suite Guest App allows you to communicate with your guests, showcase everything you have to offer, and provide that personal touch. You can:
-                    </p>
-                    <ul>
-                        <li>
-                            Front and back end development for real time reporting and automatic stock replenishment.
-                        </li>
-                        <li>
-                            Send communications out to your guests such as welcome messages, promotion specials, and updates on their booking details.
-                        </li>
-                        <li>
-                            Remove the barrier of the reception desk and create a more personalised guest experience through individual interactions.
-                        </li>
-                        <li>
-                            Live chat enabled - your guests can communicate with you on the go and receive an immediate response from your team or virtual assistant.
-                        </li>
-                        <li>
-                            Room Service - with just a few clicks of a button your guests can view menus and place orders.
-                        </li>
-                        <li>
-                            Tours and Activities - partner with local tours and activities and offer special pricing to your guests to make their stay as enjoyable as possible whilst increasing you bottom line
-                        </li>
-                        <li>
-                            Amenities - Guests can book a massage, order a newspaper and request a room clean all through your very own Xenia Guest app.
-                        </li>
-                    </ul>
-                </main>
-            </section>
-            <section className="main-section-features">
-                <div className="side">
-                    <img alt="f3-img" src={f3Img} />
-                </div>
-                <main>
-                    <h4>Improved housekeeping service</h4>
-                    <p>
-                        Monitor performance, rostering, and ensure rooms are clean and in perfect condition. Enjoy greater flexibility with housekeeping tasks and maintenance needs with our automated software.
-                    </p>
-                    <ul>
-                        <li>
-                            Create room checklists to ensure all tasks are properly completed at each clean.
-                        </li>
-                        <li>
-                            Upload images directly from the app for quality control, and lost property inventory.
-                        </li>
-                        <li>
-                            Access and track their roster and tasks via the Housekeeping App.
-                        </li>
-                        <li>
-                            Add maintenance requests
-                        </li>
-                        <li>
-                            Log incidents and communicate urgent requests through the chat function.
-                        </li>
-                    </ul>
-                </main>
-            </section>
-            <section className="main-section-features">
-                <div className="side">
-                    <img alt="f4-img" src={f4Img} />
-                </div>
-                <main>
-                    <h4>Integrated hotel back-office systems</h4>
-                    <p>
-                        Fully integrated operations and financial processes (even across multiple Properties) including full accounting, balance sheets, cash and inventory management, and cash flow visibility.
-                    </p>
-                    <ul>
-                        <li>
-                            Enjoy end to end business management through a fully integrated ERP accounting software package.
-                        </li>
-                        <li>
-                            Effectively manage reservations and point of sale including restaurant and bar, in-room charges, and events and activities requested by your guests.
-                        </li>
-                        <li>
-                            Operate multiple pricing structures and separate billing requirements for single reservations or group bookings.
-                        </li>
-                        <li>
-                            View and analyse inventory stock levels, automate purchasing and receipt of hotel goods.
-                        </li>
-                        <li>
-                            Easily manage employee information including time and attendance, rosters and payroll.
-                        </li>
-                        <li>
-                            Full reporting features to optimise your end-of-month/year-end processes.
-                        </li>
-                    </ul>
-                </main>
-            </section>
-            <section className="main-section-features">
-                <div className="side">
-                    <img alt="f5-img" src={f5Img} />
-                </div>
-                <main>
-                    <h4>Centralised customer data</h4>
-                    <p>
-                        Improve the quality and accuracy of your guest profiles including consolidated corporate
-                        information with reporting and analytics. Capture your guest’s personal preferences for
-                        that special welcome and long-term relationship.
-                    </p>
-                    <ul>
-                        <li>
-                            Access real-time data to effectively manage your reservation pipeline.
-                        </li>
-                        <li>
-                            Create personalised marketing campaigns by tailoring and understanding your guest preferences.
-                        </li>
-                        <li>
-                            Capture guest information including purpose of stay, spending habits, and seasonal trends for future marketing and communications to increase your guest loyalty.
-                        </li>
-                        <li>
-                            Easily identify when there is a downturn or quiet period so you can create special offers and promotions to increase your revenue.
-                        </li>
-                        <li>
-                            Manage your marketing campaign costs and return on investment through a complete end-to-end process.
-                        </li>
-                    </ul>
-                </main>
-            </section>
-            <section className="main-section-features">
-                <div className="side">
-                    <img alt="f6-img" src={f6Img} />
-                </div>
-                <main>
-                    <h4>Cost effective software</h4>
-                    <p>
-                        We will work with you to configure Xenia Suite to suit your specific requirements. Only
-                        pay for the functionality you need to drive your business forward.
-                    </p>
-                    <p>
-                        Xenia Suite delivers a single end-to-end solution for multiple operations allowing you to
-                        deal with one reliable vendor. Our cost effectiveness, support team, and operational
-                        efficiencies have helped our clients move from PMS legacy systems and combine their
-                        hotel functionality into one place. Want to know more? Contact us today for more
-                        information.
-                    </p>
-                </main>
-            </section>
-
         </>
     )
 }
