8ed17186d5accb754bbcb5978faf9826878269e9
Steven <StevenGongHorizonx>
2021-10-19 22:53:04 +1100

    footer postion update & hardcoded for blogs

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -27,16 +27,16 @@ export default ({ Layout, headerData }) => {
         <div className="blog-card">
           <figure>
             <a href="#">
-              <img src="https://images.unsplash.com/photo-1524006231331-78f794ebbbac?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=04ae6b2f4654598763489bbe207779cc&auto=format&fit=crop&w=500&q=60"
+              <img src="https://cdn-images-1.medium.com/max/1024/1*2GE117nagCDryS4ipIn_EA.jpeg"
                 alt="Your Blog Title" />
             </a>
           </figure>
           <div className="blog-content">
             <div className="blog-topic">
-              <a href="#">Beginner Photography</a>
+              <a href="#">Reporting</a>
             </div>
             <h3 className="blog-title">
-              <a href="#">The Top 15 Photography Tips For Absolute Beginners</a>
+              <a href="#">Report on key hotel metrics</a>
             </h3>
             <div className="blog-footer">
               <a className="blog-category" href="#">Photography</a>
@@ -57,57 +57,14 @@ export default ({ Layout, headerData }) => {
               <a href="#">Landscape Painting</a>
             </div>
             <h3 className="blog-title">
-              <a href="#">Learn How to Paint Better Landscapes With These 10 Tips</a>
+              <a href="#" dangerouslySetInnerHTML={{ __html: `\u21161 New Zealand Hotel Management Software` }} ></a>
             </h3>
             <div className="blog-footer">
-              <a className="blog-category" href="#">Painting</a>
+              <a className="blog-category" href="#">cloud-based-software</a>
               <a className="blog-date" href="#"> | a week ago</a>
             </div>
           </div>
         </div>
-
-        <div className="blog-card">
-          <figure>
-            <a href="#">
-              <img src="https://images.unsplash.com/photo-1527751171053-6ac5ec50000b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2cec328b6204439634f1365b07ef5bcb&auto=format&fit=crop&w=500&q=60"
-                alt="Your Blog Title" />
-            </a>
-          </figure>
-          <div className="blog-content">
-            <div className="blog-topic">
-              <a href="#">Chef Special</a>
-            </div>
-            <h3 className="blog-title">
-              <a href="#">This 10 Chef Blogs You Really Should be Reading</a>
-            </h3>
-            <div className="blog-footer">
-              <a className="blog-category" href="#">Cooking</a>
-              <a className="blog-date" href="#"> | 2 months ago</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="blog-card">
-          <figure>
-            <a href="#">
-              <img src="https://images.unsplash.com/photo-1480936600919-bffa6b7ecf1e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ac6548e07a9ad659ddc1ca8e80aae066&auto=format&fit=crop&w=500&q=60"
-                alt="Your Blog Title" />
-            </a>
-          </figure>
-          <div className="blog-content">
-            <div className="blog-topic">
-              <a href="#">Landscape Painting</a>
-            </div>
-            <h3 className="blog-title">
-              <a href="#">Learn How to Paint Better Landscapes With These 10 Tips</a>
-            </h3>
-            <div className="blog-footer">
-              <a className="blog-category" href="#">Painting</a>
-              <a className="blog-date" href="#"> | a week ago</a>
-            </div>
-          </div>
-        </div>
-
 
       </div>
     </Layout>
